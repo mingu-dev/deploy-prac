a10ab323d85d384e41ef7664884638f3a28f8486
mangojuicedeli <mgkang0317@gmail.com>
2020-05-21 00:54:00 +0900

    initial commit

---
+++ src/main/java/com/prac/SpringbootWebservicePracApplication.java
@@ -0,0 +1,13 @@
+package com.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicePracApplication.class, args);
+	}
+
+}
