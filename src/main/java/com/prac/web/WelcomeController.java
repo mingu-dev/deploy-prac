c8c91e99be2e8d3b74ffa2964434f71cc9f0e896
mangojuicedeli <mgkang0317@gmail.com>
2020-05-22 00:07:16 +0900

    동적 페이지 구현

--- src/main/java/com/prac/web/WelcomeController.java
+++ src/main/java/com/prac/web/WelcomeController.java
@@ -3,6 +3,7 @@ package com.prac.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,8 +12,14 @@ public class WelcomeController {
 	private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
 	
 	@GetMapping("/helloworld")
-	public String welcome() {
+	public String welcome(String name, int age, Model model) {
+		
 		log.info("/helloworld");
+		log.info("name : " + name + "age : " + age);
+		
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		
 		return "welcome";
 	}
 }
